5c1b12dcb502161ed1159d722b88be79b8a6142d
tarashevchuk0401 <tarashevchuk0401@gmail.com>
2023-03-30 17:42:58 +0200

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
-import { BikesComponent } from './bikes/bikes.component';
-import { ContactComponent } from './contact/contact.component';
-import { UserComponent } from './user/user.component';
 
-const routes: Routes = [
-  {path: '', component: AboutComponent},
-  {path: 'about', component: AboutComponent},
-  {path: 'bikes', component: BikesComponent},
-  {path: 'contact', component: ContactComponent},
-  {path: 'user', component: UserComponent},
-
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,39 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MaterialModule } from './material/material.module';
-import { LogoComponent } from './logo/logo.component';
-import { AboutComponent } from './about/about.component';
-import { BikesComponent } from './bikes/bikes.component';
-import { ContactComponent } from './contact/contact.component';
-import { UserComponent } from './user/user.component';
-import { GetContactComponent } from './get-contact/get-contact.component';
-import { FooterComponent } from './footer/footer.component';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {NgForm} from '@angular/forms';
-
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LogoComponent,
-    AboutComponent,
-    BikesComponent,
-    ContactComponent,
-    UserComponent,
-    GetContactComponent,
-    FooterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    MaterialModule,
-    HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule,
-    
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
